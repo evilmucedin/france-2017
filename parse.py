c628790b0404e931b0c9998e795088b399ee8e3d
Den Raskovalov <denplusplus@fb.com>
2017-04-25 00:40:55 -0700

    leaves extraction logic

--- parse.py
+++ parse.py
@@ -20,50 +20,66 @@ communes = 0
 
 folder = "wget/elections.interieur.gouv.fr"
 
-print("Voters\tVoted\tLe Pen\tMacron\tFilename")
+fnames = []
 for _, _, filenames in os.walk(folder):
-    for f in filenames:
-        if len(f) == 11:
-            communes += 1
-            data = open(folder + '/' + f).read()
-            mAll = re.findall(
-            """<td style="text-align:left">Inscrits</td>
+    fnames += filenames
+
+def getName(filename):
+    return filename.replace(".html", "")
+
+leaves = []
+for i in xrange(len(fnames)):
+    if i + 1 == len(fnames) or not getName(fnames[i + 1]).startswith(getName(fnames[i])):
+        if fnames[i] != "FE.html":
+            leaves.append(fnames[i])
+
+raw = True
+if raw:
+    print("Voters\tVoted\tLe Pen\tMacron\tFilename")
+
+for f in leaves:
+    communes += 1
+    data = open(folder + '/' + f).read()
+    mAll = re.findall(
+    """<td style="text-align:left">Inscrits</td>
 <td style="text-align:right">([\d ]+)</td>""", data)
-            if 1 == len(mAll):
-                cAll = str2int(mAll[0])
-                mVoted = re.findall(
-                """<td style="text-align:left">Votants</td>
+    if 1 == len(mAll):
+        cAll = str2int(mAll[0])
+        mVoted = re.findall(
+        """<td style="text-align:left">Votants</td>
 <td style="text-align:right">([\d ]+)</td>""", data)
-                if 1 == len(mVoted):
-                    cVoted = str2int(mVoted[0])
-                    if 0 != cVoted:
-                        turnout = float(cVoted)/float(cAll)
-                        mLePen = re.findall(
+        if 1 == len(mVoted):
+            cVoted = str2int(mVoted[0])
+            if 0 != cVoted:
+                turnout = float(cVoted)/float(cAll)
+                mLePen = re.findall(
 """<td style="text-align:left">Mme Marine LE PEN</td>
 <td style="text-align:right">([\d ]+)</td>""", 
 data)
-                        mMacron = re.findall(
+                mMacron = re.findall(
 """<td style="text-align:left">M. Emmanuel MACRON</td>
 <td style="text-align:right">([\d ]+)</td>""",
 data)
-                        cLePen = str2int(mLePen[0])
-                        cMacron = str2int(mMacron[0])
-                        print("%d\t%d\t%d\t%d\t%s" % (cAll, cVoted, cLePen, cMacron, f))
-                        lePen = float(cLePen)/cVoted
-                        # print("%f\t%f" % (turnout, lePen))
-                        group = int(turnout*100)
-                        if False:
-                            if group == 80:
-                                print("%s\t%f\t%f" % (f, turnout, cAll))
-                        groups[group] += 1
-                        lePenGroups[group] += lePen
-                        lePenCountGroups[group] += cLePen
-                        countGroups[group] += cAll
-                        votedGroups[cAll % 10] += 1
+                cLePen = str2int(mLePen[0])
+                cMacron = str2int(mMacron[0])
+                if raw:
+                    print("%d\t%d\t%d\t%d\t%s" % (cAll, cVoted, cLePen, cMacron, f))
+                cLePen = cMacron
+                lePen = float(cLePen)/cVoted
+                # print("%f\t%f" % (turnout, lePen))
+                group = int(turnout*100)
+                if False:
+                    if group == 80:
+                        print("%s\t%f\t%f" % (f, turnout, cAll))
+                groups[group] += 1
+                lePenGroups[group] += lePen
+                lePenCountGroups[group] += cLePen
+                countGroups[group] += cAll
+                votedGroups[cAll % 10] += 1
 
 print >>sys.stderr, "Communes = %d" % communes
 
-if False:
+if True and not raw:
     for i in xrange(101):
         if 0 != groups[i]:
             print "%f\t%f\t%f\t%f\t%f" % (i, lePenGroups[i]/groups[i], groups[i], lePenCountGroups[i], countGroups[i])
